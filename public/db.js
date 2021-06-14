9b6565d2d6386062f66a19b4fc0abf40bf650fd3
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-06-14 11:34:53 -0500

    fixed

--- public/db.js
+++ public/db.js
@@ -1,8 +1,10 @@
 let db;
-let budgetVersion;
+
+let budget;
+
 
 // Create a new db request for a "budget" database.
-const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+const request = window.indexedDB.open(budget, 1);
 
 request.onupgradeneeded = function (e) {
   console.log('Upgrade needed in IndexDB');
@@ -15,7 +17,7 @@ request.onupgradeneeded = function (e) {
   db = e.target.result;
 
   if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
+    db.createObjectStore('budgetStore', { autoIncrement: true });
   }
 };
 
@@ -27,10 +29,10 @@ function checkDatabase() {
   console.log('check db invoked');
 
   // Open a transaction on your BudgetStore db
-  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  let transaction = db.transaction(['budgetStore'], 'readwrite');
 
   // access your BudgetStore object
-  const store = transaction.objectStore('BudgetStore');
+  const store = transaction.objectStore('budgetStore');
 
   // Get all records from store and set to a variable
   const getAll = store.getAll();
@@ -52,10 +54,10 @@ function checkDatabase() {
           // If our returned response is not empty
           if (res.length !== 0) {
             // Open another transaction to BudgetStore with the ability to read and write
-            transaction = db.transaction(['BudgetStore'], 'readwrite');
+            transaction = db.transaction(['budgetStore'], 'readwrite');
 
             // Assign the current store to a variable
-            const currentStore = transaction.objectStore('BudgetStore');
+            const currentStore = transaction.objectStore('budgetStore');
 
             // Clear existing entries because our bulk add was successful
             currentStore.clear();
@@ -80,10 +82,10 @@ request.onsuccess = function (e) {
 const saveRecord = (record) => {
   console.log('Save record invoked');
   // Create a transaction on the BudgetStore db with readwrite access
-  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const transaction = db.transaction(['budgetStore'], 'readwrite');
 
   // Access your BudgetStore object store
-  const store = transaction.objectStore('BudgetStore');
+  const store = transaction.objectStore('budgetStore');
 
   // Add record to your store with add method.
   store.add(record);
